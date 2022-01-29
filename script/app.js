9f3a80ad6dcd2e39e2f67aeed5faa579bd451b97
dev.hemkanth <dev.hemkanth@gmail.com>
2022-01-29 21:15:52 +0530

    first commit

---
+++ script/app.js
@@ -0,0 +1,54 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+
+const updateUI = (data) =>  {
+    const { cityDets, weather } = data;
+
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    const timeSrc = weather.isDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc)
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc)
+
+    if (card.classList.contains('d-none')) card.classList.remove('d-none');
+}
+
+const updateCity = async (city) => {
+    
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return { cityDets, weather }
+}
+
+cityForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    updateCity(city)
+        .then(data => {
+            console.log(data);
+            updateUI(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+})
---
+++ script/forecast.js
@@ -0,0 +1,28 @@
+const key = '682500PcukwQUtq1UDd6XimUfAmBA5HL';
+
+const getWeather = async (id) => {
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/${id}`;
+    const query = `?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+// getCity('coimbatore')
+//     .then(data => {
+//         console.log(data)
+//         return getWeather(data.Key)
+//     })
+//     .catch(error => console.log(error))
